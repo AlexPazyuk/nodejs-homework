163efdc0af4b69d248f742fa31f6f6cd65606cbc
AlexPazyuk <alex.pazyuk752@gmail.com>
2024-01-30 01:27:07 +0200

    Summary

--- app.js
+++ app.js
@@ -1,30 +1,15 @@
 const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
 
 const contactsRouter = require('./routes/api/contacts');
 const usersRouter = require('./routes/api/users');
 
 
-
-
 const app = express();
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
-dotenv.config();
 
-// Mongo Connection
-mongoose.connect(process.env.MONGO_URL).then(() => {
-  console.log("Database connection successful");
-  app.listen(3000, () => {
-    console.log("Server running. Use our API on port: 3000")
-});
-}).catch((err) => {
-  console.log(err);
-  process.exit(1);
-})
 
 app.use(logger(formatsLogger));
 app.use(cors());
